08447d5f69390c0580176c4a41034c2b1e72af66
DaniSprings <danispringveldt@gmail.com>
2026-05-18 13:40:57 +0200

    git commit -m "MODIFIED VITE API URL: Client/src/services/authApi.js"

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import { createApp } from "./app.js";
 import { env } from "./config/env.js";
+import { cors } from "cors";
 
 const app = createApp();
+app.use(cors({
+  origin: [
+    "https://your-vercel-app.vercel.app",
+    "https://www.revreview.co.za"
+  ],
+  credentials: true
+}));
 
 app.listen(env.port, () => {
   console.log(`RevReview Node API listening on http://localhost:${env.port}`);
--- src/services/SupabaseVehicleservice.js
+++ src/services/SupabaseVehicleservice.js
@@ -1,3 +1,4 @@
+import { env } from 
 /**
  * supabaseVehicleService.js
  *
